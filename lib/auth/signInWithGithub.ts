194d0a175ded63a327c10609e00ad7bf64d3ac99
mio256 <reiya@woorth.io>
2024-07-18 23:27:47 +0900

    :poop:

--- lib/auth/signInWithGithub.ts
+++ lib/auth/signInWithGithub.ts
@@ -10,12 +10,17 @@ const defaultUrl = process.env.VERCEL_URL
 export default async function signInWithGithub() {
   const supabase = createClient();
 
+  console.log(`${defaultUrl}/auth/callback`);
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
       redirectTo: `${defaultUrl}/auth/callback`,
     },
   });
+
+  console.log(data);
+  console.log(error);
 
   if (error) {
     return redirect("/error");
